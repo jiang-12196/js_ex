ecc298e5745318490e4c9f42f9efffca77e5b8c4
jiang1991 <jiang12196@126.com>
2017-07-25 23:32:58 +0800

    add checkpwd fun

--- js-fun/string.js
+++ js-fun/string.js
@@ -52,3 +52,27 @@ function changeCase(str,type)
             return str;
     }
 }
+
+//checkPwd('12asdASAD')
+//3(强度等级为3)
+function checkPwd(str) {
+    var nowLv = 0;
+    if (str.length < 6) {
+        return nowLv
+    }
+    if (/[0-9]/.test(str)) {
+        nowLv++
+    }
+    if (/[a-z]/.test(str)) {
+        nowLv++
+    }
+    if (/[A-Z]/.test(str)) {
+        nowLv++
+    }
+    if (/[\.|-|_]/.test(str)) {
+        nowLv++
+    }
+    return nowLv;
+}
+
+console.log(checkPwd('23292183'));
