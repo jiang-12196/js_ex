ab458179cec887f6fff27026b3db651ce972e5a0
jiang1991 <jiang12196@126.com>
2017-10-04 03:21:49 +0800

    add app test

--- tesseract/app.js
+++ tesseract/app.js
@@ -1,9 +1,25 @@
 const tesseract = require('node-tesseract');
 
-tesseract.process('./captcha.png', function (err, text) {
+tesseract.process(__dirname + '/captcha.png', function (err, text) {
      if (err) {
          console.error(err);
      } else {
          console.log(text);
      }
+});
+
+// Recognize German text in a single uniform block of text and set the binary path
+
+var options = {
+    l: 'deu',
+    psm: 6,
+    binary: '/usr/local/bin/tesseract'
+};
+
+tesseract.process(__dirname + '/captcha.png', options, function(err, text) {
+    if(err) {
+        console.error(err);
+    } else {
+        console.log(text);
+    }
 });
