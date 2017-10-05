1e27f76d5b023bd4279d250dffb2f23c8d296001
jiang1991 <jiang12196@126.com>
2017-10-05 21:11:33 +0800

    tesseract test

--- tesseract/app.js
+++ tesseract/app.js
@@ -1,25 +1,38 @@
-const tesseract = require('node-tesseract');
+// const tesseract = require('node-tesseract');
+//
+// tesseract.process(__dirname + '/captcha.png', function (err, text) {
+//      if (err) {
+//          console.error(err);
+//      } else {
+//          console.log(text);
+//      }
+// });
+//
+// // Recognize German text in a single uniform block of text and set the binary path
+//
+// var options = {
+//     l: 'deu',
+//     psm: 6,
+//     binary: '/usr/local/bin/tesseract'
+// };
+//
+// tesseract.process(__dirname + '/captcha.png', options, function(err, text) {
+//     if(err) {
+//         console.error(err);
+//     } else {
+//         console.log(text);
+//     }
+// });
 
-tesseract.process(__dirname + '/captcha.png', function (err, text) {
-     if (err) {
-         console.error(err);
-     } else {
-         console.log(text);
-     }
-});
 
-// Recognize German text in a single uniform block of text and set the binary path
+const Tesseract = require("tesseract.js");
 
-var options = {
-    l: 'deu',
-    psm: 6,
-    binary: '/usr/local/bin/tesseract'
-};
+// Tesseract.recognize("./captcha.png")
+//     .then(function (result) {
+//         console.log("result-->",result);
+//     });
 
-tesseract.process(__dirname + '/captcha.png', options, function(err, text) {
-    if(err) {
-        console.error(err);
-    } else {
-        console.log(text);
-    }
-});
+
+Tesseract.recognize("./captcha.jmg")
+    .progress(function  (p) { console.log('progress', p)    })
+    .then(function (result) { console.log('result', result) })
