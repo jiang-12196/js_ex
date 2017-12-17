21454bc472a0429f41ae1805490c9b88418361ec
jiang1991 <jiang12196@126.com>
2017-12-17 23:24:21 +0800

    proxy img

--- js-fun/parcel/log.js
+++ js-fun/parcel/log.js
@@ -1,4 +1,23 @@
 import css from './log.css';
 
-const a = 'log';
-console.log(a);
+const myImage = (() => {
+    const imgNode = document.createElement('img');
+    document.body.appendChild(imgNode);
+    return function(src) {
+        imgNode.src = src;
+    }
+})();
+
+const proxyImage = (() => {
+    const img = new Image;
+    img.onload = function() {
+        myImage(this.src);
+    };
+
+    return function(src) {
+        myImage('http://smashinghub.com/wp-content/uploads/2014/08/cool-loading-animated-gif-3.gif');
+        img.src = src;
+    }
+})();
+
+proxyImage('http://wapfile.desktx.com/pc/160829/bigpic/5wfdpv2nbrs.gif');
