7100fa8c754769cffd7ae8b46cb4d99fccaef300
jiang1991 <jiang12196@126.com>
2018-01-06 22:26:06 +0800

    fix int fixed

--- js-fun/interview/number.js
+++ js-fun/interview/number.js
@@ -37,3 +37,22 @@ function accAdd(arg1, arg2) {
 console.log(accAdd(0.1, 0.2))
 
 console.log(accAdd(2.22, 0.1)) // 2.3200000000000003
+
+function accAddInt(arg1, arg2) {
+    var r1, r2, m;
+    try {
+        r1 = arg1.toString().split('.')[1].length
+    } catch(e) {
+        console.log(e)
+        r1 = 0
+    }
+    try {
+        r2 = arg2.toString().split('.')[1].length
+    } catch(e) {
+        r2 = 0
+    }
+    m = Math.pow(10, Math.max(r1, r2));
+    return (parseInt(arg1*m) + parseInt(arg2*m))/m
+}
+
+console.log(accAddInt(2.22, 0.1))
