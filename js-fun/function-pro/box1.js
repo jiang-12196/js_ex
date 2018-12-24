c2fc8bb54deada45ead9dd8e48d5be95091d7470
OF2919 <jiangzuohan@qianmi.com>
2018-12-24 10:56:50 +0800

    add log

--- js-fun/function-pro/box1.js
+++ js-fun/function-pro/box1.js
@@ -8,11 +8,21 @@ const f0 = x => x * 100;
 const add1 = f => x => f(x) + 1;
 const add2 = f => x => f(x) + 2;
 
-const g = Box(f0)
-  .map(f => add1(f))
-  .map(f => add2(f))
-  .fold(f => f);
+const g = Box(f0);
+const g2 = g.map(f => {
+  console.log('g2f===>', f);
+  return add1(f);
+});
+const g3 = g2.map(f => {
+  console.log('g3f===>', f);
+  return add2(f);
+});
+const g4 = g3.fold(f => f);
 
-const res = g(1);
+console.log('g: ========>', g);
+console.log('g2: ========>', g2);
+console.log('g3: ========>', g3);
+console.log('g4: ========>', g4);
+const res = g4(1);
 
 console.log(res);
